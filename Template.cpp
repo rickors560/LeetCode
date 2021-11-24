107e1818ab8085d616cdae0ee4ba1877fc8889bd
rickors560 <rickors560@gmail.com>
2021-11-25 00:35:01 +0530

    Initial Commit

---
+++ Template.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <map>
+#include <fstream>
+#include <iomanip>
+
+using namespace std;
+
+#define ll long long
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+
+    long t;
+    cout << "Start" << endl;
+    cin >> t;
+    for (long T = 0; T < t; T++)
+    {
+        cout << "Hello" << endl;
+    }
+    return 0;
+}
