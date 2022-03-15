991e07156855349bc25363445a41b0f4dc202d25
Ritik Sharma <rickors560@gmail.com>
2022-03-16 02:00:13 +0530

    Add Two Number

---
+++ AddTwoNumbers.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution
+{
+public:
+    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2)
+    {
+        ListNode *temp = new ListNode(0);
+        ListNode *ans = temp;
+
+        int carry = 0;
+        while (l1 != NULL || l2 != NULL)
+        {
+            int sum = carry;
+
+            if (l1 != NULL)
+            {
+                sum += l1->val;
+                l1 = l1->next;
+            }
+
+            if (l2 != NULL)
+            {
+                sum += l2->val;
+                l2 = l2->next;
+            }
+
+            carry = sum / 10;
+            temp->next = new ListNode(sum % 10);
+            temp = temp->next;
+        }
+
+        if (carry != 0)
+        {
+            temp->next = new ListNode(carry);
+        }
+        return ans->next;
+    }
+};
