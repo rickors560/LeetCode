f9e25e2b78545267f19da132f9a76a4bb4ff2490
Ritik Sharma <rickors560@gmail.com>
2022-03-23 22:23:03 +0530

    Broken Calculator

---
+++ BrokenCalculator.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int brokenCalc(int startValue, int target)
+    {
+        int ans = 0;
+
+        while (startValue < target)
+        {
+            if (target % 2 == 1)
+            {
+                target++;
+            }
+            else
+            {
+                target /= 2;
+            }
+            ans++;
+        }
+
+        return ans + startValue - target;
+    }
+};
