b49203d0a32740d13e7c7a06d3204a20b1f709b1
Ritik Sharma <rickors560@gmail.com>
2022-03-24 22:23:45 +0530

    Boats to save people

---
+++ BoatsToSavePeople.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int numRescueBoats(vector<int> &people, int limit)
+    {
+        sort(people.begin(), people.end());
+        int l = 0;
+        int r = people.size() - 1;
+
+        int ans = 0;
+
+        while (l <= r)
+        {
+            ans++;
+            if (people[l] + people[r] <= limit)
+            {
+                l++;
+            }
+            r--;
+        }
+
+        return ans;
+    }
+};
