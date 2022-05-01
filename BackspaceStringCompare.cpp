1af6d3393521dd026dd81e329a99b64581d5f7cf
Ritik Sharma <rickors560@gmail.com>
2022-05-01 15:04:12 +0530

    Backspace String Compare

---
+++ BackspaceStringCompare.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    bool backspaceCompare(string s, string t)
+    {
+        int i = s.size();
+        int j = t.size();
+
+        int skipS = 0;
+        int skipT = 0;
+
+        while (i >= 0 || j >= 0)
+        {
+            while (i >= 0)
+            {
+                if (s[i] == '#')
+                {
+                    skipS++;
+                    i--;
+                }
+                else if (skipS > 0)
+                {
+                    skipS--;
+                    i--;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            while (j >= 0)
+            {
+                if (t[j] == '#')
+                {
+                    skipT++;
+                    j--;
+                }
+                else if (skipT > 0)
+                {
+                    skipT--;
+                    j--;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            if (i >= 0 && j >= 0 && s[i] != t[j])
+                return false;
+
+            if (i >= 0 != j >= 0)
+                return false;
+
+            i--;
+            j--;
+        }
+
+        return true;
+    }
+};
