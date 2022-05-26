80266654e77490442ec24aa90cd0aaeb811c4f4e
Ritik Sharma <rickors560@gmail.com>
2022-05-26 22:09:57 +0530

    Number of 1 Bits

---
+++ Number Of1 Bits.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int hammingWeight(uint32_t n)
+    {
+        int result = 0;
+        while (n != 0)
+        {
+            result += n & 1;
+            n = n >> 1;
+        }
+
+        return result;
+    }
+};
