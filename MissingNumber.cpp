2047c5ae829b3fd314880b2b9c20c05e1f0c120f
Ritik Sharma <rickors560@gmail.com>
2022-05-28 19:52:34 +0530

    Missing Number

--- MissingNumber.cpp
+++ MissingNumber.cpp
@@ -4,7 +4,8 @@ using namespace std;
 class Solution
 {
 public:
-    int missingNumber(vector<int> &nums)
+    // Using XOR
+    int missingNumber1(vector<int> &nums)
     {
         int res = 0, i = 0; // a^b^b = a ->using xor property
 
@@ -13,5 +14,18 @@ public:
             res = res ^ i ^ nums[i];
         }
         return res ^ nums.size();
+    }
+
+    // Using sum
+     int missingNumber2(vector<int> &nums)
+    {
+        int n = nums.size();
+        int sum = n * (n+1)/2;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            sum -=  nums[i];
+        }
+        return sum;
     }
 };
