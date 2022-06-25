52f3e452332aa5bf88c125dc5afdd3e07f0232c9
Ritik Sharma <rickors560@gmail.com>
2022-06-25 17:01:28 +0530

    Non-decreasing Array

---
+++ Non-decreasingArray.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    bool checkPossibility(vector<int> &nums)
+    {
+        int n = nums.size();
+        bool changed = false;
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            if (nums[i] <= nums[i + 1])
+                continue;
+
+            if (changed)
+                return false;
+
+            if (i == 0 || nums[i + 1] >= nums[i - 1])
+                nums[i] = nums[i + 1];
+            else
+                nums[i + 1] = nums[i];
+            changed = true;
+        }
+
+        return true;
+    }
+};
