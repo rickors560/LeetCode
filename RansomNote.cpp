b07e66deeb0025a376d8fadae7bb699bbb1bdeba
Ritik Sharma <rickors560@gmail.com>
2022-08-26 00:27:09 +0530

    Ransom Note

---
+++ RansomNote.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    bool canConstruct(string ransomNote, string magazine)
+    {
+        int count[26] = {0};
+
+        for (int i = 0; i < magazine.size(); i++)
+            count[magazine[i] - 'a']++;
+
+        for (int i = 0; i < ransomNote.size(); i++)
+        {
+            if (count[ransomNote[i] - 'a'] == 0)
+                return false;
+
+            count[ransomNote[i] - 'a']--;
+        }
+
+        return true;
+    }
+};
