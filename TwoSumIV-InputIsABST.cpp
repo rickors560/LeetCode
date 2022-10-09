e2f80e52dd095f8194721b9c6050db02df835e81
Ritik Sharma <rickors560@gmail.com>
2022-10-09 21:09:28 +0530

    Two Sum IV - Input is a BST

---
+++ TwoSumIV-InputIsABST.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+    bool dfs(TreeNode *node, int k, unordered_set<int> &seen)
+    {
+        if (!node)
+            return false;
+
+        if (seen.count(k - node->val))
+            return true;
+
+        seen.insert(node->val);
+
+        return dfs(node->left, k, seen) || dfs(node->right, k, seen);
+    }
+
+public:
+    bool findTarget(TreeNode *root, int k)
+    {
+        unordered_set<int> seen;
+        return dfs(root, k, seen);
+    }
+};
