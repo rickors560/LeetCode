5fb2b1fb3909d3edaccd52d039d6b252d3e63d75
Ritik Sharma <rickors560@gmail.com>
2023-10-05 00:34:29 +0530

    Design Hash Map with Linked List & Bucket algo

--- DesignHashMap.cpp
+++ DesignHashMap.cpp
@@ -1,28 +1,116 @@
 #include <bits/stdc++.h>
-
 using namespace std;
+
+class Node
+{
+public:
+    int key, val;
+    Node *next;
+
+    Node(int k, int v)
+    {
+        key = k;
+        val = v;
+        next = NULL;
+    }
+};
 
 class MyHashMap
 {
-    vector<int> lookUp;
+private:
+    vector<Node *> buckets;
+    static const int numOfBuckets = 1000;
+
+    int getHash(int key)
+    {
+        return key % numOfBuckets;
+    }
 
 public:
-    MyHashMap() : lookUp(1000001, -1)
+    MyHashMap()
     {
+        buckets = vector<Node *>(numOfBuckets, NULL);
     }
 
     void put(int key, int value)
     {
-        lookUp[key] = value;
+        int hash = getHash(key);
+
+        if (buckets[hash] == NULL)
+        {
+            buckets[hash] = new Node(key, value);
+        }
+        else
+        {
+            Node *curr = buckets[hash];
+            while (curr)
+            {
+                if (curr->key == key)
+                {
+                    curr->val = value;
+                    return;
+                }
+                if (!curr->next)
+                {
+                    break;
+                }
+                curr = curr->next;
+            }
+            curr->next = new Node(key, value);
+        }
     }
 
     int get(int key)
     {
-        return lookUp[key];
+        int hash = getHash(key);
+        Node *curr = buckets[hash];
+
+        while (curr)
+        {
+            if (curr->key == key)
+            {
+                return curr->val;
+            }
+
+            curr = curr->next;
+        }
+
+        return -1;
     }
 
     void remove(int key)
     {
-        lookUp[key] = -1;
+        int hash = getHash(key);
+        Node *curr = buckets[hash];
+        Node *prev = NULL;
+
+        while (curr)
+        {
+            if (curr->key == key)
+            {
+                if (prev)
+                {
+                    prev->next = curr->next;
+                }
+                else
+                {
+                    buckets[hash] = curr->next;
+                }
+
+                delete curr;
+                return;
+            }
+
+            prev = curr;
+            curr = curr->next;
+        }
     }
 };
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
